3df1c5fbb40251e5390861521da238c83cb0f4f8
Milena <mil.lukasik@gmail.com>
2018-02-25 09:26:17 +0100

    Refactoring hard

--- SIZad1/Classes.py
+++ SIZad1/Classes.py
@@ -1,28 +1,15 @@
 import random
 import numpy as np
-
-
-def roulette(individual_list, weight_sum):
-    value = random.uniform(0.0, 1.0) * weight_sum
-    for i in individual_list:
-        value -= i.value
-        if value < 0:
-            return i
-    return individual_list[len(individual_list) - 1]
-
-
-def take_random_from_list(list_to_take, count):
-    value = []
-    random.shuffle(list_to_take)
-    for i in range(count):
-        value.append(list_to_take[i])
-
-    return value
+import plotly as plot
+import Individual
+from Generation import Generation as Gen
+from Individual import Individual as Ind
+import itertools as it
 
 
 def load_files_to_genetic_algorithm(count):
     for i in range(count):
-        with open('data' + str(i+1) + '.txt', 'r') as f:
+        with open('data' + str(i + 1) + '.txt', 'r') as f:
             matrix_dimension = int(f.readline())
             flow_matrix = np.zeros(shape=(matrix_dimension, matrix_dimension))
             distance_matrix = np.zeros(shape=(matrix_dimension, matrix_dimension))
@@ -37,91 +24,13 @@ def load_files_to_genetic_algorithm(count):
             GeneticAlgorithm.distance_matrix_list.append(distance_matrix)
 
 
-def combine_individual(individual1, individual2, combine_point_count):
-    gene_count = individual1.gene_count
-    if combine_point_count > gene_count - 1:
-        combine_point_count = gene_count - 1
-    combine_points = take_random_from_list(list(range(gene_count - 2)), combine_point_count)
-    combine_points = list(map(lambda x: x + 1, combine_points))
-    combine_points.sort()
+def take_random_from_list(list_to_take, count):
+    value = []
+    random.shuffle(list_to_take)
+    for i in range(count):
+        value.append(list_to_take[i])
 
-    new_dna1 = []
-    new_dna2 = []
-    swap = False
-    swap_counter = 0
-    for i in range(gene_count):
-        if swap:
-            new_dna1.append(individual1.dna[i])
-            new_dna2.append(individual2.dna[i])
-        else:
-            new_dna1.append(individual2.dna[i])
-            new_dna2.append(individual1.dna[i])
-            pass
-
-        if swap_counter < len(combine_points):
-            if i == combine_points[swap_counter]:
-                swap_counter += 1
-                swap = not swap
-
-    individual_list = [Individual(new_dna1, gene_count), Individual(new_dna2, gene_count)]
-    for i in individual_list:
-        if i.is_dna_incorrect():
-            i.repair_dna()
-    return individual_list
-
-
-
-def make_children(population, px, combine_point_count):
-    population_shuffle = population
-    random.shuffle(population_shuffle)
-    population_shuffle = population_shuffle[0:(px * len(population_shuffle))]
-    rest_population = population - population_shuffle
-    for i in range(len(population_shuffle)):
-        rest_population.append(combine_individual(population_shuffle[i], population_shuffle[i+1], combine_point_count))
-        i += 1
-
-    return rest_population
-
-
-class Individual:
-
-    def __init__(self, dna, gene_count):
-        self.dna = dna
-        self.gene_count = gene_count
-        self.value = None
-
-    def generate_random_dna(self):
-        self.dna = list(range(self.gene_count))
-        random.shuffle(self.dna)
-
-    '''zamienia 2 geny ze soba miejscami'''
-    def mutate(self):
-        index_of_genes_to_swap = take_random_from_list(list(range(self.gene_count)), 2)
-        gen1 = self.dna[index_of_genes_to_swap[0]]
-        self.dna[index_of_genes_to_swap[0]] = self.dna[index_of_genes_to_swap[1]]
-        self.dna[index_of_genes_to_swap[1]] = gen1
-
-    def is_dna_incorrect(self):
-        correct = list(range(self.gene_count))
-        correct.sort()
-        dna = self.dna
-        dna.sort()
-        if correct == dna:
-            return False
-        return True
-
-    def repair_dna(self):
-        counter = list(np.zeros(shape=(1, len(self.dna)))[0])
-        for i in self.dna:
-            counter[i] += 1
-
-        while counter.count(0) != 0:
-            missing_value = counter.index(0)
-            overgrow_value = counter.index(2)
-            index_of_missing_value = self.dna.index(overgrow_value)
-            self.dna[index_of_missing_value] = missing_value
-            counter[missing_value] = 1
-            counter[overgrow_value] = 1
+    return value
 
 
 class GeneticAlgorithm:
@@ -129,79 +38,101 @@ class GeneticAlgorithm:
     distance_matrix_list = []
     flow_matrix_list = []
 
-    def __init__(self, p_m, p_x, tour, pop_size, gen, combine_point_count, matrix_index):
+    def __init__(self, p_m, p_x, tour, pop_size, generations_count, combine_point_count, matrix_index):
         self.p_m = p_m
         self.p_x = p_x
         self.tour = tour
         self.pop_size = pop_size
-        self.gen = gen
+        self.generations_count = generations_count
+        '''Utworzenie macierzy, pozniejsze ich wypelnienie'''
         self.distance_matrix = self.distance_matrix_list[matrix_index]
         self.flow_matrix = self.flow_matrix_list[matrix_index]
-        self.population = []
+        '''bedziemy operowac na liscie generacji, ktora ma swoja populacje osobnikow'''
+        self.generations = []
         self.gene_count = len(self.flow_matrix[0])
         self.pop_value_sum = 0
         self.combine_point_count = combine_point_count
         self.best_of_generations = []
+        '''Zalezny wybor metody selekcji'''
         if tour == 0:
-            self.select_function = self.roulette_selection()
+            self.select_function = self.roulette_selection
         else:
-            self.select_function = self.tournament_selection(tour)
+            self.select_function = self.tournament_selection
 
     def start_evolution(self):
-        gen = 1
+        gen = 0
+        while gen != self.generations_count:
+            '''Tworze kolejna generacje, dodaje ja do listy'''
 
-        for i in range(self.pop_size):
-            self.population.append(Individual([], len(self.distance_matrix[0])))
-            self.population[i].generate_random_dna()
-
-        while gen != self.gen:
-            self.evaluate_all_individual(self)
-            self.best_of_generations.append(self.choose_alpha_individual_of_generation())
-            self.population = self.select_function
-            self.population = make_children(self.population, self.p_x, self.combine_point_count)
-            self.mutate_population()
+            if gen == 0:
+                generation = Gen(self.pop_size, self.gene_count)
+                '''Pierwsza generacje losuje'''
+                generation.generate_generation()
+                '''Tworze kolejna generacje, dodaje ja do listy'''
+                self.generations.append(generation)
+                '''Ewaluuje w niej wszystkich osobnikow'''
+                self.generations[gen].evaluate_all_individuals(self.flow_matrix, self.distance_matrix)
+            else:
+                '''Przepisuje populacje "wyzej" i na niej dzialam, potem zmieniam ja na jej dzieci'''
+                generation = self.generations[gen-1]
+                self.generations.append(generation)
+                '''Ewaluuje w niej wszystkich osobnikow'''
+                self.generations[gen].evaluate_all_individuals(self.flow_matrix, self.distance_matrix)
+                '''Podmieniam te generacje na tylko wybranych prawdopodobnych do zostania rodzicem'''
+                self.generations[gen].population = self.select_function(self.generations[gen].population, self.tour)
+                '''Podmieniam generacje na jej dzieci'''
+                self.generations[gen].population = self.generations[gen].make_children(self.p_x, self.combine_point_count)
+                '''Mutuje'''
+                self.generations[gen].mutate_population(self.p_m)
+            '''Biore sobie najlepszego z generacji'''
+            best_alpha_of_gen = self.generations[gen].choose_alpha_individual()
+            self.best_of_generations.append(best_alpha_of_gen)
+            print(gen)
+            print(len(self.generations[gen].population))
             gen += 1
 
-        print(self.best_of_generations)
+        for i in self.best_of_generations:
+            print(i.value)
         pass
 
-    def choose_alpha_individual_of_generation(self):
-        population = self.population
-        population.sort(key=lambda x: x.value , reverse=True)
-        return population[0]
-        
-    def mutate_population(self):
-        for i in self.population:
-            if random.uniform(0.0, 1.0) <= self.p_m:
-                i.mutate()
-            
-    def evaluate_all_individuals(self):
-        for i in self.population:
-            self.evaluate_individual(i)
-
-    def evaluate_individual(self, individual):
-        value = 0
-        count = individual.gene_count
-        for i in range(count-1):
-            for j in range(i + 1, count):
-                value += self.flow_matrix[i][j] * self.distance_matrix[individual.dna[i]][individual.dna[j]]
-        individual.value = 1/value
-        pass
-
-    def roulette_selection(self):
+    def roulette_selection(self, population, dummy):
         selected_individuals = []
-        weight_sum = sum(i.value for i in self.population)
-        for i in range(len(self.population)):
-            selected_individuals.append(roulette(self.population, weight_sum))
+        weight_sum = sum(i.value for i in population)
+        for i in range(len(population)):
+            selected_individuals.append(self.roulette(population, weight_sum))
         return selected_individuals
 
-    def tournament_selection(self, tour):
+    def tournament_selection(self, population, tour):
         selected_individuals = []
-        for i in range(len(self.population)):
-            population = self.population
+        for i in range(len(population)):
+            population = population
             random.shuffle(population)
             candidates = population[:tour]
             selected_individuals.append(candidates.sort(key=lambda x: x.value, reverse=True)[0])
         return selected_individuals
 
+    @staticmethod
+    def roulette(individual_list, weight_sum):
+        value = random.uniform(0.0, 1.0) * weight_sum
+        for i in individual_list:
+            value -= i.value
+            if value < 0:
+                return i
+        return individual_list[len(individual_list) - 1]
+
+    def brut_force_solution(self):
+        best = Ind([], self.gene_count)
+        best_value = 0
+        best_solution = []
+        count = 1
+        for i in it.permutations(range(self.gene_count)):
+            best.dna = i
+            best.evaluate_individual(self.flow_matrix, self.distance_matrix)
+            print(count)
+            if best.value > best_value:
+                best_solution = best.dna
+                best_value = best.value
+            count += 1
+        print(best_solution)
+        print(best_value)
 
---
+++ SIZad1/Individual.py
@@ -0,0 +1,89 @@
+import random
+import numpy as np
+import Classes as Cl
+
+class Individual:
+
+    ''' Osobnik ma swoje DNA, ilość genów oraz value, które oznacza "droge" '''
+    def __init__(self, dna, gene_count):
+        self.dna = dna
+        self.gene_count = gene_count
+        self.value = None
+
+    '''Randomowe wyznaczenie lokacji/ dna'''
+    def generate_random_dna(self):
+        '''dna to lista lokacji -> tyle ile moze byc genow'''
+        self.dna = list(range(self.gene_count))
+        random.shuffle(self.dna)
+
+    '''Wwyliczenie drogi w zaleznossci od nadrzednych danych'''
+    def evaluate_individual(self, flow_matrix, distance_matrix):
+        value = 0
+        count = self.gene_count
+        for i in range(count-1):
+            for j in range(i + 1, count):
+                value += flow_matrix[i][j] * distance_matrix[self.dna[i]][self.dna[j]]
+        self.value = 1/value
+        pass
+
+    '''zamienia 2 geny ze soba miejscami'''
+    def mutate(self):
+        index_of_genes_to_swap = Cl.take_random_from_list(list(range(self.gene_count)), 2)
+        gen1 = self.dna[index_of_genes_to_swap[0]]
+        self.dna[index_of_genes_to_swap[0]] = self.dna[index_of_genes_to_swap[1]]
+        self.dna[index_of_genes_to_swap[1]] = gen1
+
+    def is_dna_incorrect(self):
+        correct = list(range(self.gene_count))
+        correct.sort()
+        dna = self.dna
+        dna.sort()
+        if correct == dna:
+            return False
+        return True
+
+    def repair_dna(self):
+        counter = list(np.zeros(shape=(1, len(self.dna)))[0])
+        for i in self.dna:
+            counter[i] += 1
+
+        while counter.count(0) != 0:
+            missing_value = counter.index(0)
+            overgrow_value = counter.index(2)
+            index_of_missing_value = self.dna.index(overgrow_value)
+            self.dna[index_of_missing_value] = missing_value
+            counter[missing_value] = 1
+            counter[overgrow_value] = 1
+
+    '''Krzyzowanie osobnika z innym w zaleznosci od punktow krzyzowania -> zrobic wybieralne krzyzowanie'''
+    def combine_individual(self, individual2, combine_point_count):
+        gene_count = self.gene_count
+        if combine_point_count > gene_count - 1:
+            combine_point_count = gene_count - 1
+        combine_points = Cl.take_random_from_list(list(range(gene_count - 2)), combine_point_count)
+        combine_points = list(map(lambda x: x + 1, combine_points))
+        combine_points.sort()
+
+        new_dna1 = []
+        new_dna2 = []
+        swap = False
+        swap_counter = 0
+        for i in range(gene_count):
+            if swap:
+                new_dna1.append(self.dna[i])
+                new_dna2.append(individual2.dna[i])
+            else:
+                new_dna1.append(individual2.dna[i])
+                new_dna2.append(self.dna[i])
+                pass
+
+            if swap_counter < len(combine_points):
+                if i == combine_points[swap_counter]:
+                    swap_counter += 1
+                    swap = not swap
+
+        individual_list = [Individual(new_dna1, gene_count), Individual(new_dna2, gene_count)]
+        for i in individual_list:
+            if i.is_dna_incorrect():
+                i.repair_dna()
+        return individual_list

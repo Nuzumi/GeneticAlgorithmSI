62d7b8427a00ddf65135c4d2bacfd04481cbaac2
Milena <mil.lukasik@gmail.com>
2018-03-03 11:14:44 +0100

    Plot master

--- SIZad1/main.py
+++ SIZad1/main.py
@@ -7,12 +7,18 @@ import numpy as np
 
 def main():
     Cl.load_files_to_genetic_algorithm(1)
+    #start_genetic_algorithm(0.12, 0.75, 15, 100, 100, 2, 0, 50)
+    #start_genetic_algorithm_points(0.10, 0.8, 15, 100, 100, np.arange(1,10,1), 0, 50)
+    #start_genetic_algorithm_pm(np.arange(0, 0.5, 0.05), 0.75, 15, 100, 100, 2, 0, 50)
+    #start_genetic_algorithm_px(0.15, np.arange(0, 1, 0.1), 15, 100, 100, 2, 0, 50)
+    start_genetic_algorithm_tour(0.15, 0.8 , np.arange(5, 50, 5), 100, 100, 2, 0, 50)
+    start_genetic_algorithm_popsize(0.15, 0.8, 15, np.arange(50, 551, 50), 100, 2, 0, 50)
 
-    start_genetic_algorithm(0.12, 0.75, 15, 100, 100, 1, 0, 5)
     pass
 
 
 def start_genetic_algorithm(p_m, p_x, tour, pop_size, generations_count, combine_point_count, matrix_index, genetic_iterations):
+    title = Cl.GeneticAlgorithm(p_m, p_x, tour, pop_size, generations_count, combine_point_count, matrix_index)
     best_average = np.zeros(shape=(genetic_iterations, generations_count))
     average_average = np.zeros(shape=(genetic_iterations, generations_count))
     worst_average = np.zeros(shape=(genetic_iterations, generations_count))
@@ -26,11 +32,173 @@ def start_genetic_algorithm(p_m, p_x, tour, pop_size, generations_count, combine
     best_average = np.average(best_average, axis=0)
     average_average = np.average(average_average, axis=0)
     worst_average = np.average(worst_average, axis=0)
-    show_chart(generations_count, best_average, average_average, worst_average)
+    show_chart(generations_count, best_average, average_average, worst_average, title)
+
     pass
 
 
-def show_chart(x_count, y_best, y_average, y_worst):
+def start_genetic_algorithm_pm(p_m, p_x, tour, pop_size, generations_count, combine_point_count, matrix_index, genetic_iterations):
+    trace_best = np.zeros(shape=(genetic_iterations, generations_count))
+    trace_bests = []
+    axis_x = np.linspace(0, generations_count)
+
+    for i in range(len(p_m)):
+        trace_best = np.zeros(shape=(genetic_iterations, generations_count))
+        for j in range(genetic_iterations):
+            genetic = Cl.GeneticAlgorithm(p_m[i], p_x, tour, pop_size, generations_count, combine_point_count, matrix_index)
+            genetic.start_evolution()
+            trace_best[j] = genetic.best_of_generations
+
+        best_average = np.average(trace_best, axis=0)
+        trace = go.Scatter(
+                x=axis_x,
+                y=best_average,
+                mode='lines+markers',
+                name='Mutacja: ' + str(p_m[i]),
+            )
+        trace_bests.append(trace)
+
+    layout = dict(title='Wpływ parametru mutacji',
+                  xaxis=dict(title='Generacja'),
+                  yaxis=dict(title='Droga')
+                  )
+
+    chart = dict(data=trace_bests, layout=layout)
+
+    show_chart_params(chart, 'Mutacja')
+
+    pass
+
+
+def start_genetic_algorithm_px(p_m, p_x, tour, pop_size, generations_count, combine_point_count, matrix_index, genetic_iterations):
+    trace_best = np.zeros(shape=(genetic_iterations, generations_count))
+    trace_bests = []
+    axis_x = np.linspace(0, generations_count)
+
+    for i in range(len(p_x)):
+        trace_best = np.zeros(shape=(genetic_iterations, generations_count))
+        for j in range(genetic_iterations):
+            genetic = Cl.GeneticAlgorithm(p_m, p_x[i], tour, pop_size, generations_count, combine_point_count, matrix_index)
+            genetic.start_evolution()
+            trace_best[j] = genetic.best_of_generations
+
+        best_average = np.average(trace_best, axis=0)
+        trace = go.Scatter(
+                x=axis_x,
+                y=best_average,
+                mode='lines+markers',
+                name='Krzyżowanie: ' + str(p_x[i]),
+            )
+        trace_bests.append(trace)
+
+    layout = dict(title='Wpływ parametru krzyżowania',
+                  xaxis=dict(title='Generacja'),
+                  yaxis=dict(title='Droga')
+                  )
+
+    chart = dict(data=trace_bests, layout=layout)
+
+    show_chart_params(chart, 'Krzyżowanie')
+    pass
+
+
+def start_genetic_algorithm_tour(p_m, p_x, tour, pop_size, generations_count, combine_point_count, matrix_index, genetic_iterations):
+    trace_best = np.zeros(shape=(genetic_iterations, generations_count))
+    trace_bests = []
+    axis_x = np.linspace(0, generations_count)
+
+    for i in range(len(tour)):
+        trace_best = np.zeros(shape=(genetic_iterations, generations_count))
+        for j in range(genetic_iterations):
+            genetic = Cl.GeneticAlgorithm(p_m, p_x, tour[i], pop_size, generations_count, combine_point_count, matrix_index)
+            genetic.start_evolution()
+            trace_best[j] = genetic.best_of_generations
+
+        best_average = np.average(trace_best, axis=0)
+        trace = go.Scatter(
+                x=axis_x,
+                y=best_average,
+                mode='lines+markers',
+                name='Pojedynek: ' + str(tour[i]),
+            )
+        trace_bests.append(trace)
+
+    layout = dict(title='Wpływ ilości osób w pojedynku',
+                  xaxis=dict(title='Generacja'),
+                  yaxis=dict(title='Droga')
+                  )
+
+    chart = dict(data=trace_bests, layout=layout)
+
+    show_chart_params(chart, 'Tour')
+    pass
+
+
+def start_genetic_algorithm_popsize(p_m, p_x, tour, pop_size, generations_count, combine_point_count, matrix_index, genetic_iterations):
+    trace_best = np.zeros(shape=(genetic_iterations, generations_count))
+    trace_bests = []
+    axis_x = np.linspace(0, generations_count)
+
+    for i in range(len(pop_size)):
+        trace_best = np.zeros(shape=(genetic_iterations, generations_count))
+        for j in range(genetic_iterations):
+            genetic = Cl.GeneticAlgorithm(p_m, p_x, tour, pop_size[i], generations_count, combine_point_count, matrix_index)
+            genetic.start_evolution()
+            trace_best[j] = genetic.best_of_generations
+
+        best_average = np.average(trace_best, axis=0)
+        trace = go.Scatter(
+                x=axis_x,
+                y=best_average,
+                mode='lines+markers',
+                name='Populacja: ' + str(pop_size[i]),
+            )
+        trace_bests.append(trace)
+
+    layout = dict(title='Wpływ wielkości populacji',
+                  xaxis=dict(title='Generacja'),
+                  yaxis=dict(title='Droga')
+                  )
+
+    chart = dict(data=trace_bests, layout=layout)
+
+    show_chart_params(chart, 'Populacja')
+    pass
+
+
+def start_genetic_algorithm_points(p_m, p_x, tour, pop_size, generations_count, combine_point_count, matrix_index, genetic_iterations):
+
+    trace_bests = []
+    axis_x = np.linspace(0, generations_count)
+
+    for i in range(len(combine_point_count)):
+        trace_best = np.zeros(shape=(genetic_iterations, generations_count))
+        for j in range(genetic_iterations):
+            genetic = Cl.GeneticAlgorithm(p_m, p_x, tour, pop_size, generations_count, combine_point_count[i], matrix_index)
+            genetic.start_evolution()
+            trace_best[j] = genetic.best_of_generations
+
+        best_average = np.average(trace_best, axis=0)
+        trace = go.Scatter(
+                x=axis_x,
+                y=best_average,
+                mode='lines+markers',
+                name='Punkty: ' + str(combine_point_count[i]),
+            )
+        trace_bests.append(trace)
+
+    layout = dict(title='Wpływ ilości punktów krzyżowania',
+                  xaxis=dict(title='Generacje'),
+                  yaxis=dict(title='Droga')
+                  )
+
+    chart = dict(data=trace_bests, layout=layout)
+
+    show_chart_params(chart, 'Ilość punktów')
+    pass
+
+
+def show_chart(x_count, y_best, y_average, y_worst, title):
     axis_x = np.linspace(0, x_count)
     trace_best = go.Scatter(
         x=axis_x,
@@ -60,7 +228,20 @@ def show_chart(x_count, y_best, y_average, y_worst):
             color=('rgb(0, 0, 255)')
         )
     )
-    plot([trace_average, trace_best, trace_worst], 'chart.html')
+
+    layout = dict(title=str(title),
+                  xaxis=dict(title='Generacje'),
+                  yaxis=dict(title='Droga')
+                  )
+
+    chart = dict(data=[trace_average, trace_best, trace_worst], layout=layout)
+    plot(chart, 'chart.html')
+    pass
+
+
+def show_chart_params(chart, title):
+
+    plot(chart, title + '.html')
     pass
 
 
